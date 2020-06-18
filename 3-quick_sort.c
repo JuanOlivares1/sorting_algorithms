a462d04df5559ad12b2d3a1c88cf04041af8fb88
Juan Olivares <jdop2000@gmail.com>
2020-06-18 04:55:45 +0000

    3

---
+++ 3-quick_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * quick_sort - quick sort algorithm
+ * @array: array to sort
+ * @size: size of array
+ */
+void quick_sort(int *array, size_t size)
+{
+	printf("~\n");
+}
